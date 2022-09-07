9cc7a97ff7fbd99952375c9f5e77379cf1f2d4dd
Chris Johnston <cjohnston@connected.io>
2022-09-07 14:59:16 -0400

    initial implementation in elixir

---
+++ elixir/lib/bowling_game.ex
@@ -0,0 +1,33 @@
+defmodule Game do
+  def score([frame | []]) do
+    Enum.reduce(frame, 0, fn(x, acc) -> x + acc end)
+  end
+
+  def score([[10, _] | rest]) do
+    10 + next_two_rolls_score(rest) + score(rest)
+  end
+
+  def score([[roll1, roll2] | rest]) when roll1 + roll2 == 10 do
+    10 + next_roll(rest) + score(rest)
+  end
+
+  def score([frame | rest]) do
+    score([frame]) + score(rest)
+  end
+
+  def next_roll([[roll | _ ] | _]) do
+    roll
+  end
+
+  def next_two_rolls_score([[roll1, roll2, _]]) do
+    roll1 + roll2
+  end
+
+  def next_two_rolls_score([[10 | _] | rest]) do
+    10 + next_roll(rest)
+  end
+
+  def next_two_rolls_score([frame | _]) do
+    score([frame])
+  end
+end
---
+++ elixir/test/bowling_game_test.exs
@@ -0,0 +1,39 @@
+defmodule BowlingGameTest do
+  use ExUnit.Case
+
+  test "gutter rolls" do
+    assert 0 == Game.score([[0,0]])
+  end
+
+  test "1 open frame" do
+    assert 5 == Game.score([[2,3]])
+  end
+
+  test "last frame" do
+    assert 15 == Game.score([[10, 1, 4]])
+  end
+
+  test "two open frames" do
+    assert 10 == Game.score([[0, 5], [5, 0]])
+  end
+
+  test "spare" do
+    assert 21 == Game.score([[5,5], [5,1]])
+  end
+
+  test "strike" do
+    assert 20 == Game.score([[10, 0], [2,3]])
+  end
+
+  test "Two strikes" do
+    assert 42 == Game.score([[10, 0], [10, 0], [2, 3]])
+  end
+
+  test "spare and strike" do
+    assert 40 == Game.score([[4,6], [10, 0], [1, 4]])
+  end
+
+  test "perfect game" do
+    assert 300 == Game.score([[10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10, 10]])
+  end
+end
