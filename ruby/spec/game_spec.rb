4c4eb79c6d7ba0989fa96c4ff506dfa03aaad372
Chris Johnston <cjohnston@connected.io>
2022-11-23 15:19:11 -0500

    all 1s green

--- ruby/spec/game_spec.rb
+++ ruby/spec/game_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Game do
     20.times { game.roll(0) }
     expect(game.score).to eq(0)
   end
+
+  it 'rolls all 1s' do
+    game = Game.new
+    20.times { game.roll(1) }
+    expect(game.score).to eq(20)
+  end
 end
