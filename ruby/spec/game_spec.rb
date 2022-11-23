f2d0a8f1d5865499ef5088ea1a3afd2b8f845cac
Chris Johnston <cjohnston@connected.io>
2022-11-23 15:17:50 -0500

    Gutter game

--- ruby/spec/game_spec.rb
+++ ruby/spec/game_spec.rb
@@ -1,36 +1,9 @@
 require './app/game'
 
 RSpec.describe Game do
-  let(:game) { Game.new }
-
-  it 'should return 0 for a gutter game' do
+  it 'roll a gutter game' do
+    game = Game.new
     20.times { game.roll(0) }
     expect(game.score).to eq(0)
-  end
-
-  it 'can roll all ones' do
-    20.times { game.roll(1) }
-    expect(game.score).to eq(20)
-  end
-
-  it 'can roll a spare' do
-    game.roll(5)
-    game.roll(5)
-    game.roll(3)
-    17.times { game.roll(0) }
-    expect(game.score).to eq(16)
-  end
-
-  it 'can roll a strike' do
-    game.roll(10)
-    game.roll(3)
-    game.roll(4)
-    16.times { game.roll(0) }
-    expect(game.score).to eq(24)
-  end
-
-  it 'can roll a perfect game' do
-    12.times { game.roll(10) }
-    expect(game.score).to eq(300)
   end
 end
