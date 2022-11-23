4343f674cb9a6764d3bc539e4668827793d8a2a3
Chris Johnston <cjohnston@connected.io>
2022-11-23 15:19:53 -0500

    all 1s refactor

--- ruby/spec/game_spec.rb
+++ ruby/spec/game_spec.rb
@@ -1,14 +1,14 @@
 require './app/game'
 
 RSpec.describe Game do
+  let(:game) { Game.new }
+
   it 'roll a gutter game' do
-    game = Game.new
     20.times { game.roll(0) }
     expect(game.score).to eq(0)
   end
 
   it 'rolls all 1s' do
-    game = Game.new
     20.times { game.roll(1) }
     expect(game.score).to eq(20)
   end
